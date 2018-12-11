14fdac0d07b9cf285f79b1f9873a1d0e051d52d7
shfmartens <s.h.f.martens@student.tudelft.nl>
2018-12-11 14:25:13 +0100

    changed the pragma omp loop to six threads

--- src/mainExplore.cpp
+++ src/mainExplore.cpp
@@ -71,10 +71,10 @@ int main (){
     // ================================
     // == Compute manifolds ==
     // ================================
-#pragma omp parallel num_threads(18)
+#pragma omp parallel num_threads(6)
 {
     #pragma omp for
-    for (unsigned int i=0; i<18; i++) {
+    for (unsigned int i=0; i<6; i++) {
 
         std::string orbitType;
         int librationPointNr;
