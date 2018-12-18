5d3d09e92e39dbefb11672ea0214e4959861a359
shfmartens <s.h.f.martens@student.tudelft.nl>
2018-12-18 14:38:04 +0100

    adapted validation plot

--- python/phase_4/augmented_manifold_verification_and_validation.py
+++ python/phase_4/augmented_manifold_verification_and_validation.py
@@ -1137,19 +1137,23 @@ class DisplayAugmentedValidation:
             for row in self.W_S_plus.xs(i).iterrows():
                 w_s_plus_t.append(abs(row[0]))
                 w_s_plus_state = row[1].values
-                w_s_plus_mass.append(w_s_plus_state[6])
+                w_s_plus_mass_at_t = w_s_plus_state[6]
+                w_s_plus_mass.append(w_s_plus_mass_at_t)
             for row in self.W_S_min.xs(i).iterrows():
                 w_s_min_t.append(abs(row[0]))
                 w_s_min_state = row[1].values
-                w_s_min_mass.append(w_s_min_state[6])
+                w_s_min_mass_at_t = w_s_min_state[6]
+                w_s_min_mass.append(w_s_min_mass_at_t)
             for row in self.W_U_plus.xs(i).iterrows():
                 w_u_plus_t.append(abs(row[0]))
                 w_u_plus_state = row[1].values
-                w_u_plus_mass.append(w_u_plus_state[6])
+                w_u_plus_mass_at_t = w_u_plus_state[6]
+                w_s_plus_mass.append(w_u_plus_mass_at_t)
             for row in self.W_U_min.xs(i).iterrows():
                 w_u_min_t.append(abs(row[0]))
                 w_u_min_state = row[1].values
-                w_u_min_mass.append(w_u_min_state[6])
+                w_u_min_mass_at_t = w_u_min_state[6]
+                w_s_min_mass.append(w_u_min_mass_at_t)
 
             print('w_s_plus_t: ' + str(len(w_s_plus_t)))
             print('w_s_plus_mass: ' + str(len(w_s_plus_mass)))
