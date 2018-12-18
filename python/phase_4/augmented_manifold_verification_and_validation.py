a93de874f9449a7582ecc3b7ecc2e2f422280806
shfmartens <s.h.f.martens@student.tudelft.nl>
2018-12-18 15:35:06 +0100

    fixed latex bug

--- python/phase_4/augmented_manifold_verification_and_validation.py
+++ python/phase_4/augmented_manifold_verification_and_validation.py
@@ -975,7 +975,7 @@ class DisplayAugmentedValidation:
                                label='$\mathbf{X}^i_n \; \\forall \; i \in \mathcal{W}^{U-}$', linestyle='--')
 
         arr[0, 1].set_ylabel('$|x^i_{t_f} - (1-\mu)|, \; |y^i_{t_f}|$ [-]')  # \; \\forall i =0, 1, \ldots m \in \mathcal{W}
-        arr[1, 1].set_ylabel('$|y^i_{t_f}|; |x^i_{t_f} - (1-\mu)|, \$ [-]')  # \; \\forall i =0, 1, \ldots m \in \mathcal{W}
+        arr[1, 1].set_ylabel('$|y^i_{t_f}| \; |x^i_{t_f} - (1-\mu)|, \$ [-]')  # \; \\forall i =0, 1, \ldots m \in \mathcal{W}
         arr[0, 1].legend(frameon=True, loc='center left',  bbox_to_anchor=(1, 0.5))
         arr[0, 1].set_xlim([0, 1])
         arr[1, 1].set_xlim([1, 1])
