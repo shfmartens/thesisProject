0799ff2a329b3b70c075bdb20f849fc3f9f75842
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-01-07 16:38:16 +0100

    changed title of plot

--- python/phase_4/augmented_manifold_verification_and_validation.py
+++ python/phase_4/augmented_manifold_verification_and_validation.py
@@ -286,9 +286,13 @@ class DisplayAugmentedValidation:
 
         # Plot zero velocity surface
         x_range = np.arange(ax3.get_xlim()[0], ax3.get_xlim()[1], 0.001)
+        print(x_range)
         y_range = np.arange(ax3.get_ylim()[0], ax3.get_ylim()[1], 0.001)
+        print(y_range)
         x_mesh, y_mesh = np.meshgrid(x_range, y_range)
         z_mesh = cr3bp_velocity(x_mesh, y_mesh, self.C)
+        print(z_mesh)
+        
         if z_mesh.min() < 0:
             ax3.contourf(x_mesh, y_mesh, z_mesh, list(np.linspace(z_mesh.min(), 0, 10)), cmap='gist_gray_r',alpha=0.5)
 
