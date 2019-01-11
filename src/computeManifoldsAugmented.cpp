869a9d5ffc81d1959233d038b99b8e1736ed53d7
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-01-11 08:41:29 +0100

    changed thrust magnitude to 1.0E-4

--- src/computeManifoldsAugmented.cpp
+++ src/computeManifoldsAugmented.cpp
@@ -65,7 +65,7 @@ Eigen::MatrixXd retrieveSpacecraftProperties( const std::string spacecraftName)
     }
 
 
-    spacecraftProperties( 0 ) = 1.0E-3;  // Nondimensional thrust magnitude
+    spacecraftProperties( 0 ) = 1.0E-4;  // Nondimensional thrust magnitude
     spacecraftProperties( 1 ) = initialMass / initialMass; //nondimensional mass
     spacecraftProperties( 2 ) = ( -thrustMagnitude * length_asterix ) / ( specificImpulse * gravNul * time_asterix );
     spacecraftProperties( 3 ) = 0.85; //TODO,CHANGE INTO INPUT PARAMETER
