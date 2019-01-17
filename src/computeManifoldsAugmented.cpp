fe4b3779efd8b7364ffe90ac1147837348e7e0e7
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-01-17 14:14:28 +0100

    changed stopping conditions in a hacky way and mainExplore

--- src/computeManifoldsAugmented.cpp
+++ src/computeManifoldsAugmented.cpp
@@ -366,7 +366,7 @@ void computeManifoldsAugmented( const Eigen::Vector6d initialStateVector, const 
         double ySign                    = 0.0;
         double xDiffSign                = 0.0;
         //double contourCondition         = ycontourStoppingCondition(integralOfMotionOnOrbit, massParameter );
-
+        double contourCondition         = 0.5;
         //std::cout << "CONTOUR STOPPING CONDITION IS" << contourCondition << std::endl;
         offsetSign                 = offsetSigns.at(manifoldNumber);
         monodromyMatrixEigenvector = eigenVectors.at(manifoldNumber);
@@ -482,7 +482,7 @@ void computeManifoldsAugmented( const Eigen::Vector6d initialStateVector, const 
                 // Determine when the manifold crosses the Poincare section near the second primary (U2, U3)
                 if ( ((stateVectorInclSTM(0, 0) - (1.0 - massParameter)) * xDiffSign < 0) &&
                         ((librationPointNr == 1 && ( manifoldNumber == 0 || manifoldNumber == 2)) ||
-                         (librationPointNr == 2 && ( manifoldNumber == 1 || manifoldNumber == 3))) ) {
+                         (librationPointNr == 2 && ( manifoldNumber == 1 || manifoldNumber == 3))) && abs( stateVectorInclSTM(1, 0) ) < contourCondition ) {
                     reduceOvershootAtPoincareSectionU2U3Augmented(stateVectorInclSTMAndTime,
                                                          previousStateVectorInclSTMAndTime,
                                                          stateVectorInclSTM, currentTime, xDiffSign,
