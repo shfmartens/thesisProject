6328705fe6fb34f1f8830da389355fbc41d158a0
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-01-22 13:48:58 +0100

    changed python run settings

--- python/phase_4/augmented_manifold_verification_and_validation.py
+++ python/phase_4/augmented_manifold_verification_and_validation.py
@@ -1886,14 +1886,14 @@ if __name__ == '__main__':
                                                                               orbit_ids[orbit_type][lagrange_point][
                                                                                   c_level], thrust_restriction, spacecraft_name,
                                                                                   thrust_magnitude, low_dpi=low_dpi)
-                            display_augmented_validation.plot_manifolds()
-                            #display_augmented_validation.plot_manifold_zoom()
+                            #display_augmented_validation.plot_manifolds()
+                            display_augmented_validation.plot_manifold_zoom()
                             #display_augmented_validation.plot_manifold_individual()
                             #display_augmented_validation.plot_eigenvectors()
-                            #display_augmented_validation.plot_iom_validation()
-                            #display_augmented_validation.plot_stopping_validation()
-                            #display_augmented_validation.plot_thrust_validation()
-                            #display_augmented_validation.plot_mass_validation()
-                            #display_augmented_validation.plot_massrate_validation()
+                            display_augmented_validation.plot_iom_validation()
+                            display_augmented_validation.plot_stopping_validation()
+                            display_augmented_validation.plot_thrust_validation()
+                            display_augmented_validation.plot_mass_validation()
+                            display_augmented_validation.plot_massrate_validation()
 
                             del display_augmented_validation
