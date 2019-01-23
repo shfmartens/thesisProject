fa54f398b27bdacb4f6049c636ce122f3e527add
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-01-23 16:26:46 +0100

    changed run settings

--- python/phase_4/augmented_manifold_verification_and_validation.py
+++ python/phase_4/augmented_manifold_verification_and_validation.py
@@ -1868,12 +1868,12 @@ class DisplayAugmentedValidation:
 if __name__ == '__main__':
     #help()
     low_dpi = True
-    lagrange_points = [2]
+    lagrange_points = [1,2]
     orbit_types = ['horizontal']
     c_levels = [3.05, 3.1, 3.15]
-    thrust_restrictions = ['180.0']
+    thrust_restrictions = ['000.0']
     spacecraft_names = ['DeepSpace']
-    thrust_magnitudes = ['0.001000']
+    thrust_magnitudes = ['0.000000']
     orbit_ids = {'horizontal':  {1: {3.05: 808, 3.1: 577, 3.15: 330}, 2: {3.05: 1066, 3.1: 760, 3.15: 373}}}
 
     for orbit_type in orbit_types:
@@ -1886,9 +1886,9 @@ if __name__ == '__main__':
                                                                               orbit_ids[orbit_type][lagrange_point][
                                                                                   c_level], thrust_restriction, spacecraft_name,
                                                                                   thrust_magnitude, low_dpi=low_dpi)
-                            #display_augmented_validation.plot_manifolds()
-                            display_augmented_validation.plot_manifold_zoom()
-                            display_augmented_validation.plot_manifold_individual()
+                            display_augmented_validation.plot_manifolds()
+                            #display_augmented_validation.plot_manifold_zoom()
+                            #display_augmented_validation.plot_manifold_individual()
                             #display_augmented_validation.plot_eigenvectors()
                             #display_augmented_validation.plot_iom_validation()
                             #display_augmented_validation.plot_stopping_validation()
