233437177ee952ad7f3090f091bc9943ce6e4f15
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-01-31 09:50:53 +0100

    changed run settings

--- python/phase_4/augmented_manifold_verification_and_validation.py
+++ python/phase_4/augmented_manifold_verification_and_validation.py
@@ -1880,7 +1880,7 @@ if __name__ == '__main__':
     c_levels = [3.05, 3.1, 3.15]
     thrust_restrictions = ['left', 'right', '000.0', '090.0', '180.0', '270.0']
     spacecraft_names = ['DeepSpace']
-    thrust_magnitudes = ['0.000100','0.001000',,'0.010000']
+    thrust_magnitudes = ['0.000100','0.001000','0.010000']
     orbit_ids = {'horizontal':  {1: {3.05: 808, 3.1: 577, 3.15: 330}, 2: {3.05: 1066, 3.1: 760, 3.15: 373}}}
 
     for orbit_type in orbit_types:
