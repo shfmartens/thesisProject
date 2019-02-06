3df54e2696f3401da5fe864048da02080d909188
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-02-06 15:38:58 +0100

    changed scale orthographic

--- python/phase_4/augmented_manifold_verification_and_validation.py
+++ python/phase_4/augmented_manifold_verification_and_validation.py
@@ -1060,10 +1060,10 @@ class DisplayAugmentedValidation:
             w_u_plus_delta_j = []
             w_u_min_delta_j = []
             if self.thrustRestriction == "left" or self.thrustRestriction == "right" or self.thrustMagnitude == "0.000000":
-                w_s_plus_first_state = self.W_S_plus.xs(0).head(2).tail(1).values[0]
-                w_s_min_first_state = self.W_S_min.xs(0).head(2).tail(1).values[0]
-                w_u_plus_first_state = self.W_U_plus.xs(0).head(2).tail(1).values[0]
-                w_u_min_first_state = self.W_U_min.xs(0).head(2).tail(1).values[0]
+                w_s_plus_first_state = self.W_S_plus.xs(0).head(1).values[0]
+                w_s_min_first_state = self.W_S_min.xs(0).head(1).values[0]
+                w_u_plus_first_state = self.W_U_plus.xs(0).head(1).values[0]
+                w_u_min_first_state = self.W_U_min.xs(0).head(1).values[0]
                 w_s_plus_first_iom = computeIntegralOfMotion(w_s_plus_first_state[0], w_s_plus_first_state[1],
                                                         w_s_plus_first_state[2], w_s_plus_first_state[3],
                                                         w_s_plus_first_state[4], w_s_plus_first_state[5], w_s_plus_first_state[6], self.thrustMagnitude, self.thrustRestriction)
@@ -1077,10 +1077,10 @@ class DisplayAugmentedValidation:
                                                        w_u_min_first_state[2], w_u_min_first_state[3],
                                                        w_u_min_first_state[4], w_u_min_first_state[5], w_u_min_first_state[6], self.thrustMagnitude, self.thrustRestriction)
             else:
-                w_s_plus_first_state = self.W_S_plus.xs(0).head(1).values[0]
-                w_s_min_first_state = self.W_S_min.xs(0).head(1).values[0]
-                w_u_plus_first_state = self.W_U_plus.xs(0).head(1).values[0]
-                w_u_min_first_state = self.W_U_min.xs(0).head(1).values[0]
+                w_s_plus_first_state = self.W_S_plus.xs(0).head(2).tail(1).values[0]
+                w_s_min_first_state = self.W_S_min.xs(0).head(2).tail(1).values[0]
+                w_u_plus_first_state = self.W_U_plus.xs(0).head(2).tail(1).values[0]
+                w_u_min_first_state = self.W_U_min.xs(0).head(2).tail(1).values[0]
                 w_s_plus_first_iom = computeIntegralOfMotion(w_s_plus_first_state[0], w_s_plus_first_state[1],w_s_plus_first_state[2], w_s_plus_first_state[3],w_s_plus_first_state[4], w_s_plus_first_state[5],w_s_plus_first_state[6], self.thrustMagnitude,self.thrustRestriction)
                 w_s_min_first_iom = computeIntegralOfMotion(w_s_min_first_state[0], w_s_min_first_state[1],w_s_min_first_state[2], w_s_min_first_state[3],w_s_min_first_state[4], w_s_min_first_state[5],w_s_min_first_state[6], self.thrustMagnitude, self.thrustRestriction)
                 w_u_plus_first_iom = computeIntegralOfMotion(w_u_plus_first_state[0], w_u_plus_first_state[1],w_u_plus_first_state[2], w_u_plus_first_state[3],w_u_plus_first_state[4], w_u_plus_first_state[5],w_u_plus_first_state[6], self.thrustMagnitude,self.thrustRestriction)
