22254cfceda412604d7d4f62a7b7e40ac30891bf
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-02-06 11:27:26 +0100

    changed scales individual

--- python/phase_4/augmented_manifold_verification_and_validation.py
+++ python/phase_4/augmented_manifold_verification_and_validation.py
@@ -844,37 +844,37 @@ class DisplayAugmentedValidation:
 
 
         # Plot zero velocity surface
-        # x_range = np.arange(ax0.get_xlim()[0], ax0.get_xlim()[1], 0.001)
-        # y_range = np.arange(ax0.get_ylim()[0], ax0.get_ylim()[1], 0.001)
-        # x_mesh, y_mesh = np.meshgrid(x_range, y_range)
-        # z_mesh = cr3bp_velocity(x_mesh, y_mesh, self.C)
-        # if z_mesh.min() < 0:
-        #     ax0.contourf(x_mesh, y_mesh, z_mesh, list(np.linspace(z_mesh.min(), 0, 10)), cmap='gist_gray_r',
-        #                     alpha=0.5)
-        #
-        # x_range = np.arange(ax3.get_xlim()[0], ax3.get_xlim()[1], 0.001)
-        # y_range = np.arange(ax3.get_ylim()[0], ax3.get_ylim()[1], 0.001)
-        # x_mesh, y_mesh = np.meshgrid(x_range, y_range)
-        # z_mesh = cr3bp_velocity(x_mesh, y_mesh, self.C)
-        # if z_mesh.min() < 0:
-        #     ax3.contourf(x_mesh, y_mesh, z_mesh, list(np.linspace(z_mesh.min(), 0, 10)), cmap='gist_gray_r',
-        #                  alpha=0.5)
-        #
-        # x_range = np.arange(ax6.get_xlim()[0], ax6.get_xlim()[1], 0.001)
-        # y_range = np.arange(ax6.get_ylim()[0], ax6.get_ylim()[1], 0.001)
-        # x_mesh, y_mesh = np.meshgrid(x_range, y_range)
-        # z_mesh = cr3bp_velocity(x_mesh, y_mesh, self.C)
-        # if z_mesh.min() < 0:
-        #     ax6.contourf(x_mesh, y_mesh, z_mesh, list(np.linspace(z_mesh.min(), 0, 10)), cmap='gist_gray_r',
-        #                  alpha=0.5)
+        x_range = np.arange(ax0.get_xlim()[0], ax0.get_xlim()[1], 0.001)
+        y_range = np.arange(ax0.get_ylim()[0], ax0.get_ylim()[1], 0.001)
+        x_mesh, y_mesh = np.meshgrid(x_range, y_range)
+        z_mesh = cr3bp_velocity(x_mesh, y_mesh, self.C)
+        if z_mesh.min() < 0:
+            ax0.contourf(x_mesh, y_mesh, z_mesh, list(np.linspace(z_mesh.min(), 0, 10)), cmap='gist_gray_r',
+                            alpha=0.5)
 
-        # x_range = np.arange(ax9.get_xlim()[0], ax9.get_xlim()[1], 0.001)
-        # y_range = np.arange(ax9.get_ylim()[0], ax9.get_ylim()[1], 0.001)
-        # x_mesh, y_mesh = np.meshgrid(x_range, y_range)
-        # z_mesh = cr3bp_velocity(x_mesh, y_mesh, self.C)
-        # if z_mesh.min() < 0:
-        #     ax9.contourf(x_mesh, y_mesh, z_mesh, list(np.linspace(z_mesh.min(), 0, 10)), cmap='gist_gray_r',
-        #                  alpha=0.5)
+        x_range = np.arange(ax3.get_xlim()[0], ax3.get_xlim()[1], 0.001)
+        y_range = np.arange(ax3.get_ylim()[0], ax3.get_ylim()[1], 0.001)
+        x_mesh, y_mesh = np.meshgrid(x_range, y_range)
+        z_mesh = cr3bp_velocity(x_mesh, y_mesh, self.C)
+        if z_mesh.min() < 0:
+            ax3.contourf(x_mesh, y_mesh, z_mesh, list(np.linspace(z_mesh.min(), 0, 10)), cmap='gist_gray_r',
+                         alpha=0.5)
+
+        x_range = np.arange(ax6.get_xlim()[0], ax6.get_xlim()[1], 0.001)
+        y_range = np.arange(ax6.get_ylim()[0], ax6.get_ylim()[1], 0.001)
+        x_mesh, y_mesh = np.meshgrid(x_range, y_range)
+        z_mesh = cr3bp_velocity(x_mesh, y_mesh, self.C)
+        if z_mesh.min() < 0:
+            ax6.contourf(x_mesh, y_mesh, z_mesh, list(np.linspace(z_mesh.min(), 0, 10)), cmap='gist_gray_r',
+                         alpha=0.5)
+
+        x_range = np.arange(ax9.get_xlim()[0], ax9.get_xlim()[1], 0.001)
+        y_range = np.arange(ax9.get_ylim()[0], ax9.get_ylim()[1], 0.001)
+        x_mesh, y_mesh = np.meshgrid(x_range, y_range)
+        z_mesh = cr3bp_velocity(x_mesh, y_mesh, self.C)
+        if z_mesh.min() < 0:
+            ax9.contourf(x_mesh, y_mesh, z_mesh, list(np.linspace(z_mesh.min(), 0, 10)), cmap='gist_gray_r',
+                         alpha=0.5)
 
         # Plot titles for subplot
         ax0.set_title('$\mathcal{W^{S+}}$')
