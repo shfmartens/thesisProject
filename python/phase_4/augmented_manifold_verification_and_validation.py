043da27ecb79b50a346f53f8bcdb7420bdb2eb9f
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-02-07 15:28:30 +0100

    changed iom validation and individual scale

--- python/phase_4/augmented_manifold_verification_and_validation.py
+++ python/phase_4/augmented_manifold_verification_and_validation.py
@@ -1674,36 +1674,36 @@ class DisplayAugmentedValidation:
             for row in self.W_S_plus.xs(i).tail(len(self.W_S_plus.xs(i)-1)).iterrows():
                 w_s_plus_t.append(abs(row[0]))
                 w_s_plus_state = row[1].values
-                w_s_plus_time  = row[1].index[0]
+                w_s_plus_time  = row[1].index
                 w_s_plus_iom = w_s_plus_state[6]
-                w_s_plus_time_diff = abs(w_s_plus_first_time - w_s_plus_time)
+                w_s_plus_time_diff = abs(w_s_plus_first_time - w_s_plus_time[0])
                 w_s_plus_delta_j.append(abs(mass_theoretical)-(abs(w_s_plus_first_iom-w_s_plus_iom)/w_s_plus_time_diff))
                 w_s_plus_first_iom = w_s_plus_iom
                 w_s_plus_first_time = w_s_plus_time
             for row in self.W_S_min.xs(i).tail(len(self.W_S_min.xs(i)-1)).iterrows():
                 w_s_min_t.append(abs(row[0]))
                 w_s_min_state = row[1].values
-                w_s_min_time = row[1].index[0]
+                w_s_min_time = row[1].index
                 w_s_min_iom =w_s_min_state[6]
-                w_s_min_time_diff = abs(w_s_min_first_time - w_s_min_time)
+                w_s_min_time_diff = abs(w_s_min_first_time - w_s_min_time[0])
                 w_s_min_delta_j.append(abs(mass_theoretical)-abs(w_s_min_first_iom-w_s_min_iom)/w_s_min_time_diff)
                 w_s_min_first_iom = w_s_min_iom
                 w_s_min_first_time = w_s_min_time
             for row in self.W_U_plus.xs(i).tail(len(self.W_U_plus.xs(i)-1)).iterrows():
                 w_u_plus_t.append(abs(row[0]))
                 w_u_plus_state = row[1].values
-                w_u_plus_time = row[1].index[0]
+                w_u_plus_time = row[1].index
                 w_u_plus_iom = w_u_plus_state[6]
-                w_u_plus_time_diff = abs(w_u_plus_first_time - w_u_plus_time)
+                w_u_plus_time_diff = abs(w_u_plus_first_time - w_u_plus_time[0])
                 w_u_plus_delta_j.append(abs(mass_theoretical)-abs(w_u_plus_first_iom-w_u_plus_iom)/w_u_plus_time_diff)
                 w_u_plus_first_iom = w_u_plus_iom
                 w_u_plus_first_time = w_u_plus_time
             for row in self.W_U_min.xs(i).tail(len(self.W_U_min.xs(i)-1)).iterrows():
                 w_u_min_t.append(abs(row[0]))
                 w_u_min_state = row[1].values
-                w_u_min_time = row[1].index[0]
+                w_u_min_time = row[1].index
                 w_u_min_iom = w_u_min_state[6]
-                w_u_min_time_diff = abs(w_u_min_first_time - w_u_min_time)
+                w_u_min_time_diff = abs(w_u_min_first_time - w_u_min_time[0])
                 w_u_min_delta_j.append(abs(mass_theoretical)-abs(w_u_min_first_iom-w_u_min_iom)/w_u_min_time_diff)
                 w_u_min_first_iom = w_u_min_iom
                 w_u_min_first_time = w_u_min_time
