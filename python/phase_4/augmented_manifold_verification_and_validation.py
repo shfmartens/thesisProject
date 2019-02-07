565331659ba31965d6c6b520afba43e6a0c4bee0
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-02-07 13:50:37 +0100

    changed iom validation and individual scale

--- python/phase_4/augmented_manifold_verification_and_validation.py
+++ python/phase_4/augmented_manifold_verification_and_validation.py
@@ -1646,7 +1646,7 @@ class DisplayAugmentedValidation:
         w_u_min_df = pd.DataFrame(index=np.linspace(0, 100, 100 / 0.05 + 1))
 
         # self.numberOfOrbitsPerManifold
-        for i in range(9,10):
+        for i in range(29,30):
             w_s_plus_t = []
             w_s_min_t = []
             w_u_plus_t = []
