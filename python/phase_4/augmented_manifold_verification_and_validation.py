28f71048d48c363bd24301da8af506cfdab17d8a
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-02-07 15:57:47 +0100

    changed iom validation and individual scale

--- python/phase_4/augmented_manifold_verification_and_validation.py
+++ python/phase_4/augmented_manifold_verification_and_validation.py
@@ -1674,7 +1674,7 @@ class DisplayAugmentedValidation:
             for row in self.W_S_plus.xs(i).tail(len(self.W_S_plus.xs(i)-1)).iterrows():
                 w_s_plus_t.append(abs(row[0]))
                 w_s_plus_state = row[1].values
-                w_s_plus_time  = row[1].index.astype(float, copy = False)
+                w_s_plus_time  = row.index.astype(float, copy = False)
                 w_s_plus_iom = w_s_plus_state[6]
                 w_s_plus_time_diff = abs(w_s_plus_first_time - w_s_plus_time)
                 w_s_plus_delta_j.append(abs(mass_theoretical)-(abs(w_s_plus_first_iom-w_s_plus_iom)/w_s_plus_time_diff))
