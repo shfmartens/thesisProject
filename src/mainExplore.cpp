61b8a8913d79103fa89568895b4868e9c65ffd9b
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-06-10 09:48:25 +0200

    changed acceleration for equilibria generation

--- src/mainExplore.cpp
+++ src/mainExplore.cpp
@@ -31,7 +31,7 @@ int main (){
     // ================================
     // == Compute equilibria, comment out when computing low-thrust intial positions ==
     // ================================
-        double thrustAcceleration = 0.0005;
+        double thrustAcceleration = 0.001;
         createEquilibriumLocations(1, thrustAcceleration, massParameter);
         createEquilibriumLocations(2, thrustAcceleration, massParameter);
 
