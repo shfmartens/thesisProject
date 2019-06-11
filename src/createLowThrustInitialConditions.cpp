31157b6bca3349a9523144ca55df5804b48f8c35
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-06-11 13:22:28 +0200

    changed run settings

--- src/createLowThrustInitialConditions.cpp
+++ src/createLowThrustInitialConditions.cpp
@@ -468,23 +468,23 @@ Eigen::VectorXd getLowThrustInitialStateVectorGuess( const int librationPointNr,
 //        lowThrustInitialStateVectorGuess = floquetApproximation( librationPointNr, orbitType, amplitudeArray(k), initialGuessParameters(1), initialGuessParameters(2), initialGuessParameters(3), initialMass, numberOfPatchPoints );
 //    }
 
-    Eigen::ArrayXd alphaArray = Eigen::ArrayXd::LinSpaced( 360, 0, 359);
+//    Eigen::ArrayXd alphaArray = Eigen::ArrayXd::LinSpaced( 360, 0, 359);
+//    std::cout << "Array: \n" << alphaArray << std::endl;
+//    std::cout << "Array1: \n" << alphaArray(1) << std::endl;
+
+//    for(int k =0;  k < 360; k++)
+//    {
+//        lowThrustInitialStateVectorGuess = floquetApproximation( 1, orbitType, 1.0E-4, initialGuessParameters(1), alphaArray(k), initialGuessParameters(3), initialMass, numberOfPatchPoints );
+//    }
+
+    Eigen::ArrayXd accArray = Eigen::ArrayXd::LinSpaced( 91, 0.01, 0.1);
     std::cout << "Array: \n" << alphaArray << std::endl;
     std::cout << "Array1: \n" << alphaArray(1) << std::endl;
 
-    for(int k =0;  k < 360; k++)
+    for(int k =0;  k < 91; k++)
     {
-        lowThrustInitialStateVectorGuess = floquetApproximation( 1, orbitType, 1.0E-4, initialGuessParameters(1), alphaArray(k), initialGuessParameters(3), initialMass, numberOfPatchPoints );
+        lowThrustInitialStateVectorGuess = floquetApproximation( 1, orbitType, 1.0E-4, accArray(k), initialGuessParameters(2), initialGuessParameters(3), initialMass, numberOfPatchPoints );
     }
-
-    //Eigen::ArrayXd accArray = Eigen::ArrayXd::LinSpaced( 91, 0.01, 0.1);
-    //std::cout << "Array: \n" << alphaArray << std::endl;
-    //std::cout << "Array1: \n" << alphaArray(1) << std::endl;
-
-    //for(int k =0;  k < 91; k++)
-    //{
-    //    lowThrustInitialStateVectorGuess = floquetApproximation( 1, orbitType, 1.0E-4, accArray(k), initialGuessParameters(2), initialGuessParameters(3), initialMass, numberOfPatchPoints );
-    //}
 
     //lowThrustInitialStateVectorGuess = floquetApproximation( 1, orbitType, 1.0E-4, 0.01, 180, initialGuessParameters(3), initialMass, numberOfPatchPoints );
 
