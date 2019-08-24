ec579f043b8728dddf2f0df078f353c5298a4f54
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-08-24 10:38:42 +0200

    run other orbits to test whether it still comes back empty

--- src/mainExplore.cpp
+++ src/mainExplore.cpp
@@ -63,14 +63,14 @@ int main (){
     #pragma omp parallel num_threads(1)
     {
         #pragma omp for
-        for (unsigned int i=1; i<=1; i++) {
+        for (unsigned int i=1; i<=3; i++) {
             if (i ==1)
             {
                 std::cout << "Run Thread " << i << std::endl;
                 std::string orbitType = "horizontal";
                 int continuationIndex = 1; //1: Continuate for H, 8: acceleration, 9: alpha, 10: beta
                 double accelerationMagnitude = 0.1;
-                double accelerationAngle = 0.0;
+                double accelerationAngle = 90.0;
                 double accelerationAngle2 = 0.0;
                 double initialMass = 1.0;
                 createLowThrustInitialConditions(1, orbitType, continuationIndex, accelerationMagnitude, accelerationAngle, accelerationAngle2, initialMass, massParameter );
@@ -81,8 +81,8 @@ int main (){
                 std::cout << "Run Thread " << i << std::endl;
                 std::string orbitType = "horizontal";
                 int continuationIndex = 1; //1: Continuate for H, 8: acceleration, 9: alpha, 10: beta
-                double accelerationMagnitude = 0.05;
-                double accelerationAngle = 0.0;
+                double accelerationMagnitude = 0.1;
+                double accelerationAngle = 180.0;
                 double accelerationAngle2 = 0.0;
                 double initialMass = 1.0;
                 createLowThrustInitialConditions(1, orbitType, continuationIndex, accelerationMagnitude, accelerationAngle, accelerationAngle2, initialMass, massParameter );
@@ -93,8 +93,8 @@ int main (){
                 std::cout << "Run Thread " << i << std::endl;
                 std::string orbitType = "horizontal";
                 int continuationIndex = 1; //1: Continuate for H, 8: acceleration, 9: alpha, 10: beta
-                double accelerationMagnitude = 0.05;
-                double accelerationAngle = 60.0;
+                double accelerationMagnitude = 0.1;
+                double accelerationAngle = 270.0;
                 double accelerationAngle2 = 0.0;
                 double initialMass = 1.0;
                 createLowThrustInitialConditions(1, orbitType, continuationIndex, accelerationMagnitude, accelerationAngle, accelerationAngle2, initialMass, massParameter );
