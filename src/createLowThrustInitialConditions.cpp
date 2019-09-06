097c0278fdff2f33448230104b3593ec917ff9d1
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-09-06 19:40:07 +0200

    first big run

--- src/createLowThrustInitialConditions.cpp
+++ src/createLowThrustInitialConditions.cpp
@@ -1015,7 +1015,7 @@ void createLowThrustInitialConditions( const int librationPointNr, const double 
 
 // ============ CONTINUATION PROCEDURE ================== //
     // Set exit parameters of continuation procedure
-    int maximumNumberOfInitialConditions = 100;
+    int maximumNumberOfInitialConditions = 10000;
     int numberOfInitialConditions;
     if (continuationIndex == 1)
     {
