224e2121b8c70c0430af7cb65573cad731592b54
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-09-23 16:39:41 +0200

    error checking

--- src/createLowThrustInitialConditions.cpp
+++ src/createLowThrustInitialConditions.cpp
@@ -1032,7 +1032,7 @@ void createLowThrustInitialConditions( const int librationPointNr, const double 
 
 // ============ CONTINUATION PROCEDURE ================== //
     // Set exit parameters of continuation procedure
-    int maximumNumberOfInitialConditions = 100;
+    int maximumNumberOfInitialConditions = 1000;
     int numberOfInitialConditions;
     if (continuationIndex == 1)
     {
