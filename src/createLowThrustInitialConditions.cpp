9d7a8333e5c01c82365e458845ee701235c762ad
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-10-09 09:47:03 +0200

    run file until 930

--- src/createLowThrustInitialConditions.cpp
+++ src/createLowThrustInitialConditions.cpp
@@ -1101,7 +1101,7 @@ void createLowThrustInitialConditions( const int librationPointNr, const double 
 
 // ============ CONTINUATION PROCEDURE ================== //
     // Set exit parameters of continuation procedure
-    int maximumNumberOfInitialConditions = 5000;
+    int maximumNumberOfInitialConditions = 930;
     int numberOfInitialConditions;
     if (continuationIndex == 1)
     {
