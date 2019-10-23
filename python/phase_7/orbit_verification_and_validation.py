346582ce520ce02fe37053a7e3cb27ac7b96b19b
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-10-23 11:05:55 +0200

    python run settings

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1084,7 +1084,7 @@ class DisplayPeriodicSolutions:
 if __name__ == '__main__':
     orbit_types = ['horizontal']
     lagrange_points = [1]
-    acceleration_magnitudes = [0.07]
+    acceleration_magnitudes = [0.001]
     alphas = [0.0,90.0,180.0,270.0]
     betas = [0.0]
     low_dpi = True
