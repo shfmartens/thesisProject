a5441e8474da016ce76dde25321edb7612c55a56
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-10-27 15:03:48 +0100

    conditions check run

--- src/checkEigenvalues.h
+++ src/checkEigenvalues.h
@@ -6,7 +6,7 @@
 #include "Eigen/Core"
 
 
-bool checkEigenvalues( const Eigen::MatrixXd& stateVectorInclSTM, const double maxEigenvalueDeviation = 1.0E-3,
+bool checkEigenvalues( const Eigen::MatrixXd& stateVectorInclSTM, const double maxEigenvalueDeviation = 1.0E-4,
                       const bool moduleOneInsteadOfRealOne = false );
 
 
