b5d70ba1953923d06a7381edaf15c2ccc90bae31
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-11-05 09:56:45 +0100

    changed python run settings

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1250,9 +1250,9 @@ if __name__ == '__main__':
     alphas = [0,90.0,180.0,270.0]
     betas = [0.0]
     low_dpi = True
-    varying_quantities = ['xcor']
+    varying_quantities = ['Hamiltonian']
 
-    
+
 
     for orbit_type in orbit_types:
         for lagrange_point in lagrange_points:
