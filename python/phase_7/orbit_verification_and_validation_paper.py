ff63f8fb416d8699891603ed8ae1018ca7184a2a
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-11-11 13:06:05 +0100

    create ballistic paper plots

--- python/phase_7/orbit_verification_and_validation_paper.py
+++ python/phase_7/orbit_verification_and_validation_paper.py
@@ -983,10 +983,10 @@ class PeriodicSolutionsCharacterization:
 
 if __name__ == '__main__':
 
-    ballistic_planar_projection = True
-    ballistic_bifurcation_analysis = True
-    ballistic_stability_analysis = True
-    graphical_projection = False
+    ballistic_planar_projection = False
+    ballistic_bifurcation_analysis = False
+    ballistic_stability_analysis = False
+    graphical_projection = True
     bifurcation_analysis = False
     stability_analysis = False
     hamiltonian_domain_analysis = False
@@ -1003,7 +1003,7 @@ if __name__ == '__main__':
         varying_quantity = 'Hamiltonian'
         low_dpi = True
         plot_as_x_coordinate = False
-        plot_as_family_number = True
+        plot_as_family_number = False
 
         orbitL1 = DisplayPeriodicSolutions('horizontal',1,acceleration_magnitude,alpha,beta,varying_quantity,low_dpi, plot_as_x_coordinate, plot_as_family_number )
         orbitL2 = DisplayPeriodicSolutions('horizontal',2,acceleration_magnitude,alpha,beta,varying_quantity,low_dpi, plot_as_x_coordinate, plot_as_family_number )
@@ -1028,7 +1028,7 @@ if __name__ == '__main__':
         varying_quantity = 'Hamiltonian'
         low_dpi = True
         plot_as_x_coordinate = False
-        plot_as_family_number = True
+        plot_as_family_number = False
 
         orbitL1 = DisplayPeriodicSolutions('horizontal', 1, acceleration_magnitude, alpha, beta, varying_quantity,low_dpi,plot_as_x_coordinate,plot_as_family_number)
         orbitL2 = DisplayPeriodicSolutions('horizontal', 2, acceleration_magnitude, alpha, beta, varying_quantity,low_dpi,plot_as_x_coordinate,plot_as_family_number)
@@ -1053,7 +1053,7 @@ if __name__ == '__main__':
         varying_quantity = 'Hamiltonian'
         low_dpi = True
         plot_as_x_coordinate = False
-        plot_as_family_number = True
+        plot_as_family_number = False
 
 
         orbitL1 = DisplayPeriodicSolutions('horizontal', 1, acceleration_magnitude, alpha, hamiltonian, varying_quantity,low_dpi,plot_as_x_coordinate,plot_as_family_number)
@@ -1072,7 +1072,7 @@ if __name__ == '__main__':
 
     if  graphical_projection == True:
         lagrange_point_nr = 1
-        acceleration_magnitude = 0.1
+        acceleration_magnitude = 0.05
         alpha = 0.0
         beta = 0.0
         hamiltonian = 0.0
