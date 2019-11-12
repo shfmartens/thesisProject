5add95e3b420601671128597f7ab216ceb74a4f0
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-11-12 14:19:12 +0100

    changed run settings

--- src/mainExplore.cpp
+++ src/mainExplore.cpp
@@ -103,8 +103,8 @@ int main (){
                 std::cout << "Run Thread " << i << std::endl;
                 std::string orbitType = "horizontal";
                 int continuationIndex = 1; //1: Continuate for H, 6: acceleration, 7: alpha, 8: beta
-                double accelerationMagnitude = 0.01;
-                double accelerationAngle = 300.0;
+                double accelerationMagnitude = 0.05;
+                double accelerationAngle = 0.0;
                 double accelerationAngle2 = 0.00;
                 double initialMass = 1.0;
                 double ySign = 1.0;
