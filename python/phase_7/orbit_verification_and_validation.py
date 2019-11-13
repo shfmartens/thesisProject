fe3eb52f5196b1eace799a44beae4b81a93ef18d
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-11-13 20:03:45 +0100

    plots adaption

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1073,7 +1073,7 @@ class DisplayPeriodicSolutions:
 
 
 
-   
+
         arr[1,0].plot(self.orbitsId,self.accelerationContinuation,c=self.plottingColors['singleLine'], linewidth=1,label='$a_{lt}$ [-]')
         arr[1,0].set_xlim(xlim)
         arr[1,0].set_ylim([-0.01, 0.1])
@@ -1110,7 +1110,7 @@ class DisplayPeriodicSolutions:
         # added these three lines
         lns = lns0 + lns1 + lns2
         labs = [l.get_label() for l in lns]
-        arr[1, 1].legend(lns, labs, frameon=True, loc='center left', bbox_to_anchor=(1, 0.5), markerscale=15)
+        arr[1, 1].legend(lns, labs, frameon=True, loc='center left', bbox_to_anchor=(1.02, 0.5), markerscale=15)
 
         ticksLocators = [0.0, 0.5 * np.pi, np.pi, 1.5*np.pi, 2*np.pi]
         labels = ('$0$', '$ \\frac{1}{2}\\pi $', '$0$', '$ \\frac{3}{2}\\pi $', '$2 \\pi $')
