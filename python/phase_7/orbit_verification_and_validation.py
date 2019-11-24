c3c72d0d6eba65f050c1b120499f0438ca41f0c7
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-11-24 13:26:20 +0100

    adapt python orbit vv run settings

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1324,7 +1324,7 @@ class DisplayPeriodicSolutions:
 if __name__ == '__main__':
     orbit_types = ['horizontal']
     lagrange_points = [1]
-    acceleration_magnitudes = [0.01]
+    acceleration_magnitudes = [0.05]
     alphas = [0.0,60.0,120,0,180.0,240.0,300.0]
     Hamiltonians = [-1.525]
     low_dpi = True
