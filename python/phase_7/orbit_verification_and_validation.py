a75c8a75d6cdc5b24b928765e50ef81d46d10b13
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-11-25 10:48:59 +0100

    changed run settings

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1355,7 +1355,7 @@ if __name__ == '__main__':
     orbit_types = ['horizontal']
     lagrange_points = [2]
     acceleration_magnitudes = [0.05]
-    alphas = [0.0,120.0,180.0,240.0]
+    alphas = [60.0,300.0]
     Hamiltonians = [-1.525]
     low_dpi = True
     varying_quantities = ['Hamiltonian']
