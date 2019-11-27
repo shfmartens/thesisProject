2b735ec031047fadbc3d136301b9b6da55c6d786
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-11-27 13:37:13 +0100

    additional run L2 01 00

--- python/phase_7/orbit_verification_and_validation_paper.py
+++ python/phase_7/orbit_verification_and_validation_paper.py
@@ -682,7 +682,7 @@ class PeriodicSolutionsCharacterization:
         colourbar_height = position_handle[1]+position_handle[3]-colourbar_base
 
         colour_base2 = 0.073
-        colourbar_height2 = 0.94 - colour_base2
+        colourbar_height2 = 0.938 - colour_base2
         axColorbar = f.add_axes([0.92, colour_base2, 0.02, colourbar_height2])
         axColorbar.get_xaxis().set_visible(False)
         axColorbar.get_yaxis().set_visible(False)
