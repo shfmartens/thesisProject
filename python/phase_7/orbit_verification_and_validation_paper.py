a9f346ac2dfb41efc9d65e00ccf08f18c5ffa63a
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-11-28 12:42:36 +0100

    changed acc magnitude for energy rangese

--- python/phase_7/orbit_verification_and_validation_paper.py
+++ python/phase_7/orbit_verification_and_validation_paper.py
@@ -1299,7 +1299,7 @@ if __name__ == '__main__':
         del characterize_periodic_solutions
 
     if hamiltonian_domain_analysis == True:
-        lagrange_point_nr = 1
+        lagrange_point_nr = 2
         acceleration_magnitude = 0.1
         alpha = 0.0
         beta = 0.0
