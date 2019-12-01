09c209269c95777092d86513f564d079d619e703
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-12-01 16:45:56 +0100

    removed pragma omp

--- src/applyCollocation.cpp
+++ src/applyCollocation.cpp
@@ -1024,6 +1024,8 @@ Eigen::VectorXd applyCollocation(const Eigen::MatrixXd initialCollocationGuess, 
                                                           double maxPositionDeviationFromPeriodicOrbit,  double maxVelocityDeviationFromPeriodicOrbit,  double maxPeriodDeviationFromPeriodicOrbit, const int maxNumberOfCollocationIterations, const double maximumErrorTolerance)
 {
 
+    std::cout << "Apply collocation reached" << std::endl;
+
     // ======= initialize variables and rewrite input for the collocation procedure ====== //
     Eigen::VectorXd outputVector = Eigen::VectorXd::Zero(27);
     Eigen::VectorXd outputDesignVector;
