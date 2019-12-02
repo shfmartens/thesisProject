6bc3551a9da03008736ce8cb8630f8ce08f8cc93
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-12-02 13:35:22 +0100

    changed run settings

--- src/mainExplore.cpp
+++ src/mainExplore.cpp
@@ -120,7 +120,7 @@ int main (){
                 double familyHamiltonian = -1.55;
                 int numberOfFamilyMembers = 1000;
                 bool startContinuationFromTextFile = false;
-                createLowThrustInitialConditions(2, ySign, orbitType, continuationIndex, accelerationMagnitude, accelerationAngle, accelerationAngle2, initialMass, familyHamiltonian, startContinuationFromTextFile, numberOfFamilyMembers );
+                createLowThrustInitialConditions(1, ySign, orbitType, continuationIndex, accelerationMagnitude, accelerationAngle, accelerationAngle2, initialMass, familyHamiltonian, startContinuationFromTextFile, numberOfFamilyMembers );
 
 
 
