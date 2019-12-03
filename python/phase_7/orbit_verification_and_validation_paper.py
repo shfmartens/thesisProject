cb6411245cb9bdda37cca573a3b3b6bd22825741
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-12-03 20:43:38 +0100

    changed python run settings

--- python/phase_7/orbit_verification_and_validation_paper.py
+++ python/phase_7/orbit_verification_and_validation_paper.py
@@ -1285,7 +1285,7 @@ if __name__ == '__main__':
         acceleration_magnitude = 0.01
         alpha = 0.0
         beta = 0.0
-        hamiltonian = -1.525
+        hamiltonian = -1.50
         varying_quantity = 'Acceleration'
         low_dpi = False
         plot_as_x_coordinate = False
@@ -1325,7 +1325,7 @@ if __name__ == '__main__':
         acceleration_magnitude = 0.01
         alpha = 0.0
         beta = 0.0
-        hamiltonian = -1.525
+        hamiltonian = -1.50
         varying_quantity = 'Acceleration'
         low_dpi = False
         plot_as_x_coordinate = False
@@ -1363,7 +1363,7 @@ if __name__ == '__main__':
         acceleration_magnitude = 0.01
         alpha = 0.0
         beta = 0.0
-        hamiltonian = -1.525
+        hamiltonian = -1.50
         varying_quantity = 'Acceleration'
         low_dpi = False
         plot_as_x_coordinate = False
