03e6e6cb1b60ab7b762b9339d0f4695c0114dd3d
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-12-03 13:52:38 +0100

    changed run settings

--- src/mainExplore.cpp
+++ src/mainExplore.cpp
@@ -103,10 +103,10 @@ int main (){
     // ================================
         
 
-    #pragma omp parallel num_threads(1)
+    #pragma omp parallel num_threads(5)
     {
         #pragma omp for
-        for (unsigned int i=1; i<=1; i++) {
+        for (unsigned int i=1; i<=5; i++) {
             if (i ==1)
             {
                 std::cout << "Run Thread " << i << std::endl;
