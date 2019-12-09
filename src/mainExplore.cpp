b72c5f8d5cefb1395547a97edcd462c6e3f5a72b
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-12-09 19:08:41 +0100

    generate alt var fam for alpha

--- src/mainExplore.cpp
+++ src/mainExplore.cpp
@@ -103,10 +103,10 @@ int main (){
     // ================================
         
 
-    #pragma omp parallel num_threads(4)
+    #pragma omp parallel num_threads(1)
     {
         #pragma omp for
-        for (unsigned int i=1; i<=4; i++) {
+        for (unsigned int i=1; i<=1; i++) {
             if (i ==1)
             {
                 std::cout << "Run Thread " << i << std::endl;
