384988c5e2cbf136bccc208091e3d6f205176c39
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-12-10 10:06:54 +0100

    fix increment

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1424,9 +1424,9 @@ class DisplayPeriodicSolutions:
 
 if __name__ == '__main__':
     orbit_types = ['horizontal']
-    lagrange_points = [1]
+    lagrange_points = [2]
     acceleration_magnitudes = [0.1]
-    alphas = [120.0,240.0]
+    alphas = [60.0,300.0]
     Hamiltonians = [-1.55]
     low_dpi = False
     varying_quantities = ['Hamiltonian']
