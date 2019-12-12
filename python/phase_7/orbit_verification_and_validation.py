e757f5b670dc73b8a6b975e2cc01ef72c8bd1c63
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-12-12 15:00:58 +0100

    alpha varying new batch

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1581,7 +1581,7 @@ if __name__ == '__main__':
     low_dpi = False
     varying_quantities = ['Hamiltonian']
     plot_as_x_coordinate  = False
-    plot_as_family_number = False
+    plot_as_family_number = True
 
 
 
