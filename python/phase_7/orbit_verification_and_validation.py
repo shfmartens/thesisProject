abcdbdfcdb36df721633730f1b6e3df5cede4391
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-12-12 11:45:58 +0100

    alpha varying new batch

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1436,9 +1436,9 @@ class DisplayPeriodicSolutions:
 
 if __name__ == '__main__':
     orbit_types = ['horizontal']
-    lagrange_points = [2]
-    acceleration_magnitudes = [0.05]
-    alphas = [120,240]
+    lagrange_points = [1]
+    acceleration_magnitudes = [0.1]
+    alphas = [120]
     Hamiltonians = [-1.50]
     low_dpi = False
     varying_quantities = ['Hamiltonian']
