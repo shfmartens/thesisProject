7e5c708f6ee70e53aea0abd351e55df7abb4b478
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-12-12 19:20:46 +0100

    alpha varying new batch

--- src/mainExplore.cpp
+++ src/mainExplore.cpp
@@ -106,7 +106,7 @@ int main (){
     #pragma omp parallel num_threads(4)
     {
         #pragma omp for
-        for (unsigned int i=1; i<=4; i++) {
+        for (unsigned int i=1; i<=1; i++) {
             if (i ==1)
             {
                 std::cout << "Run Thread " << i << std::endl;
