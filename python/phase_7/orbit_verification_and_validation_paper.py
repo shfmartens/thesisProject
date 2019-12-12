6ecfd044e4f681869073b95592a0d7a8b0061540
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-12-12 19:50:56 +0100

    alpha varying new batch

--- python/phase_7/orbit_verification_and_validation_paper.py
+++ python/phase_7/orbit_verification_and_validation_paper.py
@@ -1307,7 +1307,7 @@ if __name__ == '__main__':
         del characterize_periodic_solutions
 
     if  graphical_projection == True:
-        lagrange_point_nr = 1
+        lagrange_point_nr = 2
         acceleration_magnitude = 0.0
         alpha = 0.0
         beta = 0.0
