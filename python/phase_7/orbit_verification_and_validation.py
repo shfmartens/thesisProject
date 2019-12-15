f9cd0fb9046528c97bd2d91253d794f29b6930c4
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-12-15 16:23:47 +0100

    changed plot titles

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1638,9 +1638,9 @@ class DisplayPeriodicSolutions:
 
 if __name__ == '__main__':
     orbit_types = ['horizontal']
-    lagrange_points = [1,2]
-    acceleration_magnitudes = [0.0]
-    alphas = [0.0,0.0]
+    lagrange_points = [1]
+    acceleration_magnitudes = [0.01]
+    alphas = [120.0,240.0]
     Hamiltonians = [-1.55]
     low_dpi = False
     varying_quantities = ['Hamiltonian']
