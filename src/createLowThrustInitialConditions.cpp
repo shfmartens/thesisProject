fec9cea14d8bc797a0302518f83d42f497dea62e
shfmartens <s.h.f.martens@student.tudelft.nl>
2019-12-16 12:35:47 +0100

    run new alpha vars

--- src/createLowThrustInitialConditions.cpp
+++ src/createLowThrustInitialConditions.cpp
@@ -1662,9 +1662,9 @@ void createLowThrustInitialConditions( const int librationPointNr, const double 
                {
                    for(int j = 0; j < 4; j++)
                    {
-                       if( oddNodesMatrix(11*i+continuationIndex,j) + incrementTest > 0.0995)
+                       if( oddNodesMatrix(11*i+continuationIndex,j) + incrementTest > 0.0495)
                        {
-                            oddNodesMatrix(11*i+continuationIndex,j) = 0.1;
+                            oddNodesMatrix(11*i+continuationIndex,j) = 0.05;
                        } else {
 
                            oddNodesMatrix(11*i+continuationIndex,j) = oddNodesMatrix(11*i+continuationIndex,j) + incrementTest;
@@ -1888,7 +1888,7 @@ void createLowThrustInitialConditions( const int librationPointNr, const double 
 
 
 
-                if (continuationIndex == 6 && stateVectorInclSTM(6,0) > 0.0995)
+                if (continuationIndex == 6 && stateVectorInclSTM(6,0) > 0.0495)
                 {
 
                     std::cout << "termination condition stateVectorInclSTM(0,6) > 0.1 reached: "  << std::endl;
