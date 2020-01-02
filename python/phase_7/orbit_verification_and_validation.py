c4256b58363be2dff01448ddb87e8ac5335f9e05
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-02 14:16:02 +0100

    reproduce plots vv

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1650,7 +1650,7 @@ if __name__ == '__main__':
     lagrange_points = [2]
     acceleration_magnitudes = [0.1]
     alphas = [60.0,300]
-    Hamiltonians = [-1.55]
+    Hamiltonians = [-1.50]
     low_dpi = False
     varying_quantities = ['Acceleration']
     plot_as_x_coordinate  = False
