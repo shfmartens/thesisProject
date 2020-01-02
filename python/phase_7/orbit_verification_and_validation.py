d8f2e6dd3a6c0fe88d901739a6bffe6d7c894c6c
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-02 13:50:38 +0100

    reproduce plots vv

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1647,10 +1647,10 @@ class DisplayPeriodicSolutions:
 
 if __name__ == '__main__':
     orbit_types = ['horizontal']
-    lagrange_points = [1,2]
+    lagrange_points = [2]
     acceleration_magnitudes = [0.1]
     alphas = [60.0,300]
-    Hamiltonians = [-1.55]
+    Hamiltonians = [-1.50]
     low_dpi = False
     varying_quantities = ['Acceleration']
     plot_as_x_coordinate  = False
