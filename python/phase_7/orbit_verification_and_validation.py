c685a2b5529b60cc753127e8f55c352d515110d9
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-03 18:35:37 +0100

    ballistic pimping

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1840,7 +1840,7 @@ if __name__ == '__main__':
     orbit_types = ['horizontal']
     lagrange_points = [1]
     acceleration_magnitudes = [0.1]
-    alphas = [60.0]
+    alphas = [240.0]
     Hamiltonians = [-1.55]
     low_dpi = False
     varying_quantities = ['Hamiltonian']
