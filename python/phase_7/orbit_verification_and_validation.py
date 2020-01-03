70b952a9961cfd33aa9fbe43c3097b14d7f182aa
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-03 19:17:36 +0100

    ballistic pimping

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -613,8 +613,8 @@ class DisplayPeriodicSolutions:
             counter_temp = counter_temp + 1
 
         if self.alpha == 300:
-            print('lambda2: final ' + str(self.lambda2[1012]))
-            print('lambda5: final ' + str(self.lambda5[1012]))
+            print('lambda2: final ' + str(self.lambda2[1013]))
+            print('lambda5: final ' + str(self.lambda5[1013]))
 
 
 
