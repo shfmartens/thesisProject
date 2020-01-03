b65b1915a4e245f9464ac25e2cb3f207019b2cc7
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-03 16:59:21 +0100

    ballistic pimping

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1722,7 +1722,7 @@ class DisplayPeriodicSolutions:
 
 if __name__ == '__main__':
     orbit_types = ['horizontal']
-    lagrange_points = [1]
+    lagrange_points = [2]
     acceleration_magnitudes = [0.05]
     alphas = [120.0]
     Hamiltonians = [-1.55]
