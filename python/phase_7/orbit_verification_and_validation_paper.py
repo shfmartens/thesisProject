21c694bce47ebba8f448f040b97546472fbeaadd
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-03 14:09:09 +0100

    ballistic pimping

--- python/phase_7/orbit_verification_and_validation_paper.py
+++ python/phase_7/orbit_verification_and_validation_paper.py
@@ -1454,10 +1454,6 @@ if __name__ == '__main__':
         characterize_periodic_solutions.graphical_projection()
         characterize_periodic_solutions.bifurcation_analysis()
         characterize_periodic_solutions.stability_analysis()
-        #characterize_periodic_solutions.hamiltonian_domain_analysis()
-
-
-
 
         del characterize_periodic_solutions
 
