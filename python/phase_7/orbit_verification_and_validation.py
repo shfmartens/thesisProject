52ce5fdfa7dd99c68e5347e299026d17fab99fcd
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-04 13:55:45 +0100

    resolving bifurcation diagrams

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1965,7 +1965,7 @@ if __name__ == '__main__':
     orbit_types = ['horizontal']
     lagrange_points = [1]
     acceleration_magnitudes = [0.10]
-    alphas = [300.0]
+    alphas = [180.0]
     Hamiltonians = [-1.55]
     low_dpi = False
     varying_quantities = ['Hamiltonian']
