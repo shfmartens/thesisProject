cb824ec29e757fb0eea54b3970282c6aca9dd8cf
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-04 12:22:37 +0100

    resolving bifurcation diagrams

--- python/phase_7/orbit_verification_and_validation_paper.py
+++ python/phase_7/orbit_verification_and_validation_paper.py
@@ -1416,8 +1416,8 @@ if __name__ == '__main__':
         del characterize_periodic_solutions
 
     if  graphical_projection == True:
-        lagrange_point_nr = 1
-        acceleration_magnitude = 0.1
+        lagrange_point_nr = 2
+        acceleration_magnitude = 0.05
         alpha = 0.0
         beta = 0.0
         hamiltonian = -1.50
