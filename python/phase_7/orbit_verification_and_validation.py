64045809b627b229ffd58eb0a350d37f3e3b640f
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-05 17:11:45 +0100

    bifurcation resolving

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -298,11 +298,11 @@ class DisplayPeriodicSolutions:
             idx_out_plane = []
 
 
-            # if counter_temp == 1011:
-            #     print ('family member: ' + str(counter_temp))
-            #     print ('M: ' + str(M))
-            #     print('eigenvalues: ' + str(eigenvalue))
-            #     print('sorting_indices: ' + str(sorting_indices))
+            if counter_temp == 2036:
+                print ('family member: ' + str(counter_temp))
+                print ('M: ' + str(M))
+                print('eigenvalues: ' + str(eigenvalue))
+                print('sorting_indices: ' + str(sorting_indices))
 
             # if counter_temp > 2035 and counter_temp < 2039:
             #     print('member ' + str(counter_temp) + ' eigenvalues: ' + str(eigenvalue))
