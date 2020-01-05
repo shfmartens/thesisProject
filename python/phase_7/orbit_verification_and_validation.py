86afc631734d8ef3dd09baaf607f8f041910c887
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-05 15:34:11 +0100

    bifurcation resolving

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -685,7 +685,7 @@ class DisplayPeriodicSolutions:
                 # Find indices of the first pair of real eigenvalue equal to one
                 for idx, l in enumerate(eigenvalue):
                     if abs(l.imag) < self.maxEigenvalueDeviation:
-                        if abs(l.real - 1.0) < self.maxEigenvalueDeviation:
+                        if abs(l.real - 1.0) < 3.0*self.maxEigenvalueDeviation:
                             if sorting_indices[2] == -1:
                                 sorting_indices[2] = idx
                                 idx_in_plane.append(idx)
