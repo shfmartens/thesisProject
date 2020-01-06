01d3e5ab6d6295f3d83d3f8eca6c2b3540e7595b
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-06 16:53:11 +0100

    quantification appendix extract info from families

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -2339,7 +2339,7 @@ class DisplayPeriodicSolutions:
 if __name__ == '__main__':
     orbit_types = ['horizontal']
     lagrange_points = [1]
-    acceleration_magnitudes = [0.05]
+    acceleration_magnitudes = [0.1]
     alphas = [0.0,60.0,120.0,180.0,240.0,300.0]
     Hamiltonians = [-1.525]
     low_dpi = False
