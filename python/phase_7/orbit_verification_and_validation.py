a79e898f54d5ab242f0b28a23d788bdd99e974b9
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-06 14:08:40 +0100

    quantification appendix extract info from families

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -2345,7 +2345,7 @@ if __name__ == '__main__':
     low_dpi = False
     varying_quantities = ['Hamiltonian']
     plot_as_x_coordinate  = False
-    plot_as_family_number = True
+    plot_as_family_number = False
 
 
     for orbit_type in orbit_types:
