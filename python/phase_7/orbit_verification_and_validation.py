c4ce3ce3bfa3f7f9c742d0ea41a690e618654339
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-07 15:14:36 +0100

    change angle from 60 to 300

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -2346,7 +2346,7 @@ class DisplayPeriodicSolutions:
 
 if __name__ == '__main__':
     orbit_types = ['horizontal']
-    lagrange_points = [1]
+    lagrange_points = [2]
     acceleration_magnitudes = [0.1]
     alphas = [0.0,60.0,120.0,180.0,240.0,300.0]
     Hamiltonians = [-1.55]
