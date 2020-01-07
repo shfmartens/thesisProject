cf268c0c8838f1d4321fe7ea9df0d7a6d35d692a
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-07 11:16:59 +0100

    checking bifurcations

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -298,7 +298,7 @@ class DisplayPeriodicSolutions:
             idx_out_plane = []
 
 
-            if counter_temp > 1695 and counter_temp < 1703:
+            if counter_temp > 1967 and counter_temp < 1972:
                 print ('family member: ' + str(counter_temp))
                 print ('M: ' + str(M))
                 print('eigenvalues: ' + str(eigenvalue))
