2cfe1ec4e0ffed0aed57e5f34ad29f37a520d4c7
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-07 10:05:00 +0100

    checking bifurcations

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -2340,7 +2340,7 @@ class DisplayPeriodicSolutions:
 
 if __name__ == '__main__':
     orbit_types = ['horizontal']
-    lagrange_points = [1]
+    lagrange_points = [2]
     acceleration_magnitudes = [0.1]
     alphas = [120.0]
     Hamiltonians = [-1.525]
