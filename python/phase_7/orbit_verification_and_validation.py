aa9d9c75e248e31c18288f7857ccecbf505c5384
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-07 15:06:39 +0100

    change angle from 60 to 300

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -2348,7 +2348,7 @@ if __name__ == '__main__':
     orbit_types = ['horizontal']
     lagrange_points = [1]
     acceleration_magnitudes = [0.1]
-    alphas = [0.0]
+    alphas = [0.0,60.0,120.0,180.0,240.0,300.0]
     Hamiltonians = [-1.55]
     low_dpi = False
     varying_quantities = ['Acceleration']
