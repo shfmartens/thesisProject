68dbc9e7fed60b5977168c6c07b3589799ea5a95
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-09 14:59:31 +0100

    test presence of alpha-varying families

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -2347,7 +2347,7 @@ class DisplayPeriodicSolutions:
 if __name__ == '__main__':
     orbit_types = ['horizontal']
     lagrange_points = [2]
-    acceleration_magnitudes = [0.05]
+    acceleration_magnitudes = [0.01]
     alphas = [0.0]
     Hamiltonians = [-1.50]
     low_dpi = False
