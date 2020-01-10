d1b36abfda152ab5fcbca00d1e77880cf3d3a7ef
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-10 11:20:58 +0100

    cahnged stability index

--- python/phase_7/orbit_verification_and_validation_paper.py
+++ python/phase_7/orbit_verification_and_validation_paper.py
@@ -1418,8 +1418,8 @@ if __name__ == '__main__':
         del characterize_periodic_solutions
 
     if  graphical_projection == True:
-        lagrange_point_nr = 2
-        acceleration_magnitude = 0.05
+        lagrange_point_nr = 1
+        acceleration_magnitude = 0.1
         alpha = 0.0
         beta = 0.0
         hamiltonian = -1.55
