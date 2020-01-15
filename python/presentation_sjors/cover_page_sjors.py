4953b2256def162f26ded16fba770d6116d76279
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-15 11:06:01 +0100

    changed cover picture

--- python/presentation_sjors/cover_page_sjors.py
+++ python/presentation_sjors/cover_page_sjors.py
@@ -98,8 +98,8 @@ class presentationAnimations:
         self.figureRatioSix = (7 * (1 + np.sqrt(5)) / 2) * 2/ (7*3)
 
         # Title figure dimensions
-        self.figSizeTitle = (7 , 7*1.5)
-        self.figureRatioTitle = (7, 7*1.5)
+        self.figSizeTitle = (7.0 , 7.0*1.5)
+        self.figureRatioTitle = (7.0)/ (7.0*1.5)
 
 
         # figure with two subplots next to eachother
