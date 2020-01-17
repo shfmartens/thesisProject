7ec7ed1c735b92be9a5fb7cafce48417433acbcd
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-17 11:43:54 +0100

    test different lengths shooting symmetry

--- python/phase_7/orbit_verification_and_validation_paper.py
+++ python/phase_7/orbit_verification_and_validation_paper.py
@@ -1522,7 +1522,7 @@ class PeriodicSolutionsCharacterization:
             Hlt_min = 0.0
             Hlt_max = 360.0
 
-            print(Orbit1.alpha)
+            print(Orbit1.alphaContinuation)
 
             continuation_normalized_orbit1 = [(value - Hlt_min) / (Hlt_max - Hlt_min) for value in
                                               Orbit1.continuationParameter]
