45536aff07e7b37d81dcf503c66fb50e1e0af02e
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-17 18:22:09 +0100

    change quant info

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -1767,7 +1767,7 @@ class DisplayPeriodicSolutions:
         print('Minimum v1 ' + str(min(self.v1)) + ' at member: ' + str(self.v1.index(min(self.v1))))
         print('Maximum v1 ' + str(max(self.v1)) + ' at member: ' + str(self.v1.index(max(self.v1))))
 
-        print('alpha Cont: ' +str(self.alphaContinuation))
+        #print('alpha Cont: ' +str(self.alphaContinuation))
         # print('local extrema max: ' + str(max(self.continuationParameter[800:1500])) + ' at member: ' + str(self.continuationParameter[800:1500].index(max(self.continuationParameter[800:1500]))+800))
         # print('local extrema minimum: ' + str(min(self.continuationParameter[1000:4449])) + ' at member: ' + str(self.continuationParameter[1000:4449].index(min(self.continuationParameter[1000:4449]))+1500))
         #
