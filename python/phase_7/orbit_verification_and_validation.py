e2e8dcf2243e918ecb35592249466c53d7d21fc3
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-20 17:34:03 +0100

    change quant info

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -2370,7 +2370,7 @@ if __name__ == '__main__':
     lagrange_points = [2]
     acceleration_magnitudes = [0.1]
     alphas = [0.0,60.0,120.0,180.0,240.0,300.0]
-    Hamiltonians = [-1.55]
+    Hamiltonians = [-1.525]
     low_dpi = False
     varying_quantities = ['Acceleration']
     plot_as_x_coordinate  = False
