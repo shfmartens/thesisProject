bf3b8f97d2c9981591bec40c9ce14cfb18e4ae6b
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-21 16:53:33 +0100

    change quant info

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -2369,7 +2369,7 @@ if __name__ == '__main__':
     orbit_types = ['horizontal']
     lagrange_points = [2]
     acceleration_magnitudes = [0.1]
-    alphas = [60.0,120.0,240.0,300.0]
+    alphas = [180.0]
     Hamiltonians = [-1.55]
     low_dpi = False
     varying_quantities = ['Hamiltonian']
