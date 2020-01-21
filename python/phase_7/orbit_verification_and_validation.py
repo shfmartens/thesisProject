882896ea6ee606b708edeafc6c515528ec88fdf7
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-21 15:49:10 +0100

    change quant info

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -2367,9 +2367,9 @@ class DisplayPeriodicSolutions:
 
 if __name__ == '__main__':
     orbit_types = ['horizontal']
-    lagrange_points = [2]
-    acceleration_magnitudes = [0.05]
-    alphas = [120.0,240.0]
+    lagrange_points = [1]
+    acceleration_magnitudes = [0.1]
+    alphas = [120.0]
     Hamiltonians = [-1.55]
     low_dpi = False
     varying_quantities = ['Hamiltonian']
