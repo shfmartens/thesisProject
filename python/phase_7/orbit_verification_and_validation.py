837c91ff8b43d8dea94113f5c8265a09e82b1df4
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-24 13:46:12 +0100

    changed xlims

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -2379,9 +2379,9 @@ class DisplayPeriodicSolutions:
 
 if __name__ == '__main__':
     orbit_types = ['horizontal']
-    lagrange_points = [1]
+    lagrange_points = [2]
     acceleration_magnitudes = [0.1]
-    alphas = [120.0,240]
+    alphas = [060.0,300]
     Hamiltonians = [-1.55]
     low_dpi = False
     varying_quantities = ['Hamiltonian']
