a1644ee7ed2d404a27f4f957281651557b62c4ff
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-01-26 09:43:21 +0100

    test xcoords

--- python/phase_7/orbit_verification_and_validation.py
+++ python/phase_7/orbit_verification_and_validation.py
@@ -2389,7 +2389,7 @@ if __name__ == '__main__':
     lagrange_points = [1]
     acceleration_magnitudes = [0.1]
     alphas = [0.0]
-    Hamiltonians = [-1.525]
+    Hamiltonians = [-1.50]
     low_dpi = False
     varying_quantities = ['Alpha']
     plot_as_x_coordinate  = False
