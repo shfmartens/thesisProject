cd6c9cee74477ca8106910e028017890587eba25
shfmartens <s.h.f.martens@student.tudelft.nl>
2020-02-10 16:31:33 +0100

    edit plot

--- python/phase_7/orbit_verification_and_validation_paper.py
+++ python/phase_7/orbit_verification_and_validation_paper.py
@@ -1189,7 +1189,7 @@ class PeriodicSolutionsCharacterization:
             arr[1].set_xticks([0, 90, 180, 270, 360])
             arr[1].set_xticklabels(['$0$', '$\\frac{1}{2}\\pi$', '$\\pi$', '$\\frac{3}{2}\\pi$', '$2\\pi$'])
 
-        f.subplots_adjust(left=0.06,bottom=0.23,top=0.89)
+        f.subplots_adjust(left=0.06,bottom=0.15,top=0.89)
 
 
         lgd = arr[1].legend(frameon=True, loc='center left', bbox_to_anchor=(1.03, 0.5), prop={'size': 8})
@@ -1847,7 +1847,7 @@ if __name__ == '__main__':
         characterize_periodic_solutions = PeriodicSolutionsCharacterization(lagrange_point_nr, acceleration_magnitude,
                                                                             alpha, hamiltonian, varying_quantity, my_objects,low_dpi,plot_as_x_coordinate,plot_as_family_number)
 
-        characterize_periodic_solutions.graphical_projection()
+        #characterize_periodic_solutions.graphical_projection()
         #characterize_periodic_solutions.bifurcation_analysis()
         characterize_periodic_solutions.stability_analysis()
 
